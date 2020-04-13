35d0ba85efe3f8213a7d6f8869847364ce2a4f3c
Zedekia Malawi <jatelo68@gmail.com>
2020-04-13 16:49:22 -0400

    updating file

---
+++ Master/index.js
@@ -0,0 +1,109 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const convertFactory = require('electron-html-to');
+
+// const generateHTML = require('./generateHTML.js')
+
+var conversion = convertFactory({
+    converterPath: convertFactory.converters.PDF
+});
+
+
+let data = {};
+
+let questions = [
+    {
+        message: 'What is your github username?',
+        name: 'username',
+    },
+    {
+        message: 'What is your favorite color',
+        name: 'color',
+        type: 'list',
+        choices: ['green', 'blue', 'pink', 'red'],
+    }
+]
+
+
+// function writeToFile(fileName, data) {
+
+// } not necessary with electron-html-to
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(function ({ username, color }) {
+            const queryUrl = `https://api.github.com/users/${username}`;
+
+            axios
+                .get(queryUrl)
+                .then((res) => {
+                    // console.log(res.data)
+
+
+
+                    switch (color) {
+                        case 'green':
+                            data.color = 0;
+                            break;
+                        case 'blue':
+                            data.color = 1;
+                            break;
+                        case 'pink':
+                            data.color = 2;
+                            break;
+                        case 'red':
+                            data.color = 3;
+                            break;
+                    }
+                    // console.log(data.color)  
+
+                    data.username = username;
+                    data.numOfRepo = res.data.public_repos;
+                    data.name = res.data.name
+                    data.followers = res.data.followers;
+                    data.following = res.data.following;
+                    data.portPic = res.data.avatar_url;
+                    data.location = res.data.location;
+                    data.blog = res.data.blog;
+                    data.company = res.data.company
+                    data.bio = res.data.bio
+
+                    axios // Requires a different axios call to get stars
+                        .get(`https://api.github.com/users/${username}/repos?per_page=100`)
+                        .then((res) => {
+                            // console.log(res)
+                            data.stars = 0;
+                            for (let i = 0; i < res.data.length; i++) { // Loop through each repository and count the number of stars
+                                data.stars += res.data[i].stargazers_count;
+                            }
+
+
+                            // console.log(data.stars)
+
+                            let resumeHTML = generateHTML(data);
+                            // console.log(resumeHTML)
+
+                            conversion({ html: resumeHTML }, function (err, result) {
+                                if (err) {
+                                    return console.error(err);
+                                }
+
+                                console.log(result.numberOfPages);
+                                console.log(result.logs);
+                                result.stream.pipe(fs.createWriteStream('./resume.pdf'));
+                                conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+                            });
+                        })
+
+
+
+
+
+
+                })
+        })
+}
+
+init();
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Developer-Profile-Generator
+AS A product manager
+
+I WANT a developer profile generator
+
+SO THAT I can easily prepare reports for stakeholders
+```
